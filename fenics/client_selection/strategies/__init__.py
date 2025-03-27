1c33dccb637241df137d11906f3383a9033a39d2
Shubham-Saha <53710056+Shubham-Saha@users.noreply.github.com>
2025-03-27 11:41:53 +0100

    Update __init__.py

--- fenics/client_selection/strategies/__init__.py
+++ fenics/client_selection/strategies/__init__.py
@@ -1,8 +1,8 @@
 # fenics/client_selection/strategies/__init__.py
-from fenics.client_selection.strategies.uniform import select_clients_uniform
+# from fenics.client_selection.strategies.uniform import select_clients_uniform
 from fenics.client_selection.strategies.md_sampling import select_clients_md_sampling
 
 __all__ = [
-    'select_clients_uniform',
+    # 'select_clients_uniform',
     'select_clients_md_sampling'
 ]
