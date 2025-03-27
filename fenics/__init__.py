3fb09addebf46b649d01c99015899af6613bcbed
Shubham-Saha <53710056+Shubham-Saha@users.noreply.github.com>
2025-03-27 11:57:03 +0100

    Update __init__.py

--- fenics/__init__.py
+++ fenics/__init__.py
@@ -1,19 +1,21 @@
 # fenics/__init__.py
 
 __version__ = "1.3.2"
-__author__ = "Shubham Saha, Sifat Nawrin Nova"
-__email__ = "shuvsaha7@gmail.com, nawrinnova04@gmail.com"
+__author__ = 
+__email__ = 
 
 # Import key components from modules
 from fenics.cli import FenicsShell, run_fenics_shell
 from fenics.config import parse_arguments, SimulationConfig, load_config_from_file, parse_arguments_pydantic
-from fenics.models import Net, ModelBase, MLP, ModelFactory
+# from fenics.models import Net, ModelBase, MLP, ModelFactory
+from fenics.models import Net, ModelBase, ModelFactory
 from fenics.data import load_datasets_dirichlet, print_class_distribution, DataModule
 from fenics.topology import create_nodes, build_topology, visualize_and_save_topology, TopologyFactory
 from fenics.training import local_train, evaluate, summarize_model_parameters
 from fenics.communication import send_update, gossip_step, neighboring_step, CommunicationProtocol, ProtocolFactory
 from fenics.aggregation import FedAvgStrategy, AggregationStrategy, AggregationFactory
-from fenics.client_selection import ClientSelector, select_clients_uniform, select_clients_md_sampling, SelectionFactory
+# from fenics.client_selection import ClientSelector, select_clients_uniform, select_clients_md_sampling, SelectionFactory
+from fenics.client_selection import ClientSelector, select_clients_md_sampling, SelectionFactory
 from fenics.utils import setup_logging, calculate_selection_probabilities, detect_convergence
 from fenics.simulator import Simulator
 from fenics.attack import AttackManager, Attack, PoisonAttack, DelayAttack, AttackFactory
@@ -33,7 +35,7 @@ __all__ = [
     # Models
     'Net',
     'ModelBase',
-    'MLP',
+    # 'MLP',
     'ModelFactory',
 
     # Data
@@ -66,7 +68,7 @@ __all__ = [
     
     # Client Selection
     'ClientSelector',
-    'select_clients_uniform',
+    # 'select_clients_uniform',
     'select_clients_md_sampling',
     'SelectionFactory',
 
