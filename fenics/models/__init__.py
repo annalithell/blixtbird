c18207d28d5198eea7e53a4ff10d688af7b398f2
Shubham-Saha <53710056+Shubham-Saha@users.noreply.github.com>
2025-03-27 11:48:23 +0100

    Update __init__.py

--- fenics/models/__init__.py
+++ fenics/models/__init__.py
@@ -2,12 +2,12 @@
 
 from fenics.models.cnn import Net
 from fenics.models.base import ModelBase
-from fenics.models.mlp import MLP
+# from fenics.models.mlp import MLP
 from fenics.models.factory import ModelFactory
 
 __all__ = [
     'Net',
     'ModelBase',
-    'MLP',
+    # 'MLP',
     'ModelFactory'
 ]
