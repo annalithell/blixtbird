32d2fc92ea1a179d5b77e5b8e89e6f4ef2e0344a
Shubham-Saha <53710056+Shubham-Saha@users.noreply.github.com>
2025-03-28 22:21:12 +0100

    Update selector.py

--- fenics/client_selection/selector.py
+++ fenics/client_selection/selector.py
@@ -5,7 +5,7 @@ import random
 import logging
 from typing import List, Dict, Optional
 
-from fenics.client_selection.strategies.uniform import select_clients_uniform
+# from fenics.client_selection.strategies.uniform import select_clients_uniform
 from fenics.client_selection.strategies.md_sampling import select_clients_md_sampling
 
 
