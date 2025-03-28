6ada91225a3dcb84e838a3ec75b690e9f3c750d3
Shubham-Saha <53710056+Shubham-Saha@users.noreply.github.com>
2025-03-28 22:22:37 +0100

    Update factory.py

--- fenics/client_selection/factory.py
+++ fenics/client_selection/factory.py
@@ -3,7 +3,7 @@
 from typing import Dict, Callable, List, Optional
 import logging
 
-from fenics.client_selection.strategies.uniform import select_clients_uniform
+# from fenics.client_selection.strategies.uniform import select_clients_uniform
 from fenics.client_selection.strategies.md_sampling import select_clients_md_sampling
 
 
