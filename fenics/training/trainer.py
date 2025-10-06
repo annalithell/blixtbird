c652f6c90bd07839e5c3a215b6d96b5ac789be2b
zaidh <zaidh@chalmers.se>
2025-10-06 16:53:09 +0200

    cuda line

--- fenics/training/trainer.py
+++ fenics/training/trainer.py
@@ -20,7 +20,7 @@ def local_train(node_id, local_model, train_dataset, epochs, attacker_type):
     Returns:
         Tuple of (model state dictionary, training time)
     """
-    device = torch.device("cpu")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = local_model
     model.to(device)
     # Added weight decay for L2 regularization
