1246037275c0dff26fe7f1ed18cdb6d547dda428
annalithell <89861494+annalithell@users.noreply.github.com>
2025-10-21 16:47:17 +0200

    Freerider: No training happens - shown in plots

--- fenics/node/attacks/freerider.py
+++ fenics/node/attacks/freerider.py
@@ -7,6 +7,7 @@ from typing import Optional, override
 import time
 import numpy as np
 
+from torchvision import datasets, transforms
 from fenics.node.attacks.attack_node import AttackNode
 from fenics.node.attacks.attack_registry import register_attack
 
@@ -44,40 +45,17 @@ class FreeRiderAttack(AttackNode):
         #self.logger.info(f"[Free-rider node {self.node_id}] fakes training...")
         print((f"[Free-rider node {self.node_id}] fakes training..."))
 
+        # Create test DataLoader
+        transform = transforms.Compose([transforms.ToTensor()])
+        test_dataset = datasets.FashionMNIST('./data', train=False, download=True, transform=transform)
+        test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=32, shuffle=False)
+
         #  Use random metrics
-        self.append_training_metrics()
-        self.append_test_metrics()
+        self.append_training_metrics(train_loader)
+        self.append_test_metrics(test_loader)
 
         training_time = time.time() - start_time
-        return self.model.state_dict(), training_time # NOT NEEDED??
-
-    def append_training_metrics(self):
-        # Evaluation phase: training data
-        train_loss = np.random.random(1)[0]
-        train_accuracy = np.random.random(1)[0]
-        train_f1 = np.random.random(1)[0]
-        train_precision = np.random.random(1)[0]
-        train_recall = np.random.random(1)[0]
-
-        self.metrics_train.append({'train_loss': train_loss,
-                                'train_accuracy': train_accuracy,
-                                'train_f1_score': train_f1,
-                                'train_precision': train_precision,
-                                'train_recall':train_recall})
-    
-    def append_test_metrics(self):
-        # Evaluation phase: testing data
-        loss = np.random.random(1)[0]
-        accuracy = np.random.random(1)[0]
-        f1 = np.random.random(1)[0]
-        precision = np.random.random(1)[0]
-        recall = np.random.random(1)[0]
-
-        self.metrics_test.append({'test_loss': loss,
-                                'test_accuracy': accuracy,
-                                'test_f1_score': f1,
-                                'test_precision': precision,
-                                'test_recall': recall})
+        return self.model.state_dict(), training_time
 
 
     def execute(self):
